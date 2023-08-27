0511de983bd4488ecf23a7ff534d37556a3c4149
Oleksandr_Andriianov <vipedron@gmail.com>
2023-08-27 14:23:25 +0300

    make tests as xfail

--- tests/test_lambdatest.py
+++ tests/test_lambdatest.py
@@ -4,7 +4,7 @@ import pytest
 from utils.file_utils import read_data_file
 
 
-@pytest.xfail
+@pytest.mark.xfail(reason="Bug in the API")
 @allure.suite("Lambdatest API tests")
 @allure.title("JSON to XML conversion")
 @pytest.mark.parametrize("file_name", ["1", "2"])
@@ -22,7 +22,7 @@ def test_json_to_xml(lambdatest_service, file_name):
         assert mini_actual_xml == mini_expected_xml
 
 
-@pytest.xfail
+@pytest.mark.xfail(reason="Bug in the API")
 @allure.suite("Lambdatest API tests")
 @allure.title("Extract text from JSON")
 @allure.severity(allure.severity_level.CRITICAL)
